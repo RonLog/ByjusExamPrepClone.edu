a2ac60bbe1ce81be7e009e35fbcf9e794da7a5d9
RonLog <roshan7715singh@gmail.com>
2023-01-27 13:33:56 +0530

    iconchange color respective url

--- js/main.js
+++ js/main.js
@@ -6,3 +6,17 @@ window.onload = () => {
             .register('./sw.js');
     }
 }
+
+let currentUrlPage = document.URL;
+if(currentUrlPage = "https://ronlog.github.io/BujusExamPrepClone.edu" ){
+    document.getElementsByClassName("fa-house-chimney").style.color = "blueviolet";
+}
+else if(currentUrlPage = "https://ronlog.github.io/BujusExamPrepClone.edu/ByjusPractice.html" ){
+    document.getElementsByClassName("fa-book-open").style.color = "blueviolet";
+}
+else if(currentUrlPage = "https://ronlog.github.io/BujusExamPrepClone.edu/byjusTestSeries.html"){
+    document.getElementsByClassName("fa-paste").style.color = "blueviolet";
+}
+else {
+    document.getElementsByClassName("fa-video").style.color = "blueviolet";
+}
